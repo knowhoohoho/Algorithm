11fb193114e28ea4534af7bc168940470e197ca1
knowhoohoho <kljm789@naver.com>
2023-02-06 01:47:07 +0900

    .

--- Level0/32.특이한 정렬/test.js
+++ Level0/32.특이한 정렬/test.js
@@ -1,7 +1,16 @@
-function solution(list, n) {
-  return list.sort((a,b) => {
-     const [i,j] = [Math.abs(a-n), Math.abs(b-n)]
-     if(i == j) return b-a
-     return i-j
-  })
-}
+// function aa (list, num) {
+//   return list.sort((i,j) => {
+//      const [o,p] =[Math.abs(i-num), Math.abs(j-num)]
+//      console.log('o ::', o,'[[',p)
+//     if(o ==p) return j-i
+//     return o-p
+//   }) 
+// }
+// aa([1,2,3,4,5,], 4)
+/**
+ * sort를 사용해서 정렬을 할거고 math.abs를 사용해서   i,j에 주어진 수 num의 절대값을 추려내고 
+ * 그 값이 값으면 더 큰 수를 반환해야하기 때문에 j-i를 했고 그게 아니라면 오름차순으로 리턴
+ */
+
+
+
