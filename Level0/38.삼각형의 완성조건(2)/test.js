201af2d9cfb4fb3e4283144f15b0b6d75cb040ff
knowhoohoho <kljm789@naver.com>
2023-02-10 00:53:33 +0900

    삼각형의 완성조건(2)

---
+++ Level0/38.삼각형의 완성조건(2)/test.js
@@ -0,0 +1,31 @@
+const a= [11, 7]
+
+function solution(list) {
+   const max = Math.max(...list)
+   const min = Math.min(...list)
+   let arr = [];
+
+   //주어진 값에 max값이 있을때
+   for(let i =1; i<=max; i++) {
+     if(min + i > max) {
+        arr.push(i)
+     }
+   }
+
+   //주어진 값에 max값이 없을때
+   for(let i=1; i<(max + min); i++) {
+      if(max < i) arr.push(i)
+   }
+return arr.length
+}
+
+
+/**
+ * 다른 사람들의 문제 풀이 
+ *
+ *  정말 대단한거 같다 ...
+ *  
+ */
+  // function solution(list) {
+  //   return Math.min(...list)*2 -1
+  // }
