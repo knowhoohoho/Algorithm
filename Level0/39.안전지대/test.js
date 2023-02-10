109d021add785693e807ab8083221ed9fdc95a31
knowhoohoho <kljm789@naver.com>
2023-02-10 23:23:15 +0900

    안전지대

---
+++ Level0/39.안전지대/test.js
@@ -0,0 +1,13 @@
+const a =[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]
+
+function solution(list) {
+  for(let i=0; i<list.length; i++) {
+    for(let j=0; j<list[i].length; j++) {
+      
+      //보류
+    }
+  }
+}
+
+
+solution(a)
