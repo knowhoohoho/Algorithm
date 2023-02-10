e3c8759918fb6cccce71cfbdb1790a3448829ea6
knowhoohoho <kljm789@naver.com>
2023-02-11 01:08:15 +0900

    숨어있는 숫자의 덧셈(2)

--- Level0/40.숨어있는 숫자의 덧셈(2)/test.js
+++ Level0/40.숨어있는 숫자의 덧셈(2)/test.js
@@ -1,15 +1,28 @@
-function solution(list) {
-  let result = 0
-  for(let i=0; i <list.length; i++) {
-      let tmp ='';
+// function solution(list) {
+//   let result = 0
+//   for(let i=0; i <list.length; i++) {
+//       let tmp ='';
       
-      //!isNaN만 사용해도 괜찮을 거 같지만 mdn에서 Number.isNan을 권장함.
-      while(!Number.isNaN(+(list[i]))) {
-          tmp += list[i]
-          i ++
-          console.log(tmp)
-      }
-      result += +tmp
-  }
-  return result                                                                                                                                                                         
+//       //!isNaN만 사용해도 괜찮을 거 같지만 mdn에서 Number.isNan을 권장함.
+//       while(!Number.isNaN(+(list[i]))) {
+//           tmp += list[i]
+//           i ++
+//           console.log(tmp)
+//       }
+//       result += +tmp
+//   }
+//   return result                                                                                                                                                                         
+// }
+
+
+/**
+ *  다른 사람의 풀이 
+ *  정규식을 사용해서 문자열이면 공백으로 바꿔준다.
+ *  
+ */
+
+function solution(list) {
+  return list.replace(/[a-zA-Z]/g, " ").split(' ').filter(x => x != '').reduce((i,j) => parseInt(i) + parseInt(j))
 }
+
+
