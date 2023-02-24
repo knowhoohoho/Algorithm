37dbc59b2ae8373a63f00027324a7839864d86fe
knowhoohoho <kljm789@naver.com>
2023-02-24 18:31:18 +0900

    배열 회전시키기

--- Level0/55.배열 회전시키기/test.js
+++ Level0/55.배열 회전시키기/test.js
@@ -1,11 +1,6 @@
-const a = [1, 2, 3]
-const b = "right"
-
 function solution(list, n) {
   if(n == 'right') list.unshift(list.pop())
   if(n == 'left') list.push(list.shift())
   return list
 }
 
-
-console.log(solution(a,b))
