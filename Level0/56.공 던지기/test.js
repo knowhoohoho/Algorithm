8f2383a392611f44f0469161aa63e99e94632f7f
knowhoohoho <kljm789@naver.com>
2023-02-24 18:52:09 +0900

    공 던지기

---
+++ Level0/56.공 던지기/test.js
@@ -0,0 +1,9 @@
+function solution(numbers, k) {
+  let num = 0
+  for(let i = 1; i < k; i++){
+      num += 2;
+      if(num > numbers.length) num %= numbers.length
+      console.log('aa',num%= numbers.length)
+  }
+  return numbers[num];
+}
